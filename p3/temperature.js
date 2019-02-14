489c6081874d984858623f3fa329fe3603829ff6
Fanchen Bao <jonathonbao@gmail.com>
2019-02-13 21:37:56 -0500

    updated temperature.js to include check for user input and refractored functions

--- p3/temperature.js
+++ p3/temperature.js
@@ -1,13 +1,28 @@
-const report = function (celsius, fahrenheit) {
-  document.getElementById("result").innerHTML = celsius + "\xb0C = " + fahrenheit + "\xb0F";
+const report = function (res) {
+  document.getElementById("result").innerHTML = res;
 };
 
 document.getElementById("f_to_c").onclick = function () {
-  let f = document.getElementById("temperature").value;
-  report((f - 32) / 1.8, f);
+  let f = Number(document.getElementById("temperature").value);
+  if (checkValid(f))
+    report(buildRes(FtoC(f), f));
+  else
+    report("Please enter a number.");
 };
 
 document.getElementById("c_to_f").onclick = function () {
-  let c = document.getElementById("temperature").value;
-  report(c, 1.8 * c + 32);
+  let c = Number(document.getElementById("temperature").value);
+  if (checkValid(c))
+    report(buildRes(c, CtoF(c)));
+  else
+    report("Please enter a number.");
 };
+
+const FtoC = f => (f - 32) / 1.8;
+
+const CtoF = c => 1.8 * c + 32;
+
+// check whether the given value val is a number
+const checkValid = val => !Number.isNaN(val);
+
+const buildRes = (c, f) => String(c.toFixed(2)) + "\xb0C = " + String(f.toFixed(2)) + "\xb0F";
