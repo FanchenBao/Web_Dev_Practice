46163e2e0a81b16868965b4ef228a54346a8883e
Fanchen Bao <jonathonbao@gmail.com>
2019-04-08 00:23:51 -0400

    first commit for p6, experimenting stage with mysql and form handling

---
+++ p6/login.php
@@ -0,0 +1,10 @@
+<?php // login.php
+  $hn = 'localhost';
+  $db = 'test';
+  $un = 'root'; // Change this
+  $pw = 'root'; // Change this
+
+  $conn = new mysqli($hn, $un, $pw, $db);
+  if ($conn->connect_error) die("Fatal Error connecting to database.");
+?>
+
